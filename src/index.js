8b0dc49f74c875baefa7ea272f0417347e504a18
Sophia <sophierithous@gmail.com>
2020-04-18 08:33:38 +0100

    filter and loader

--- src/index.js
+++ src/index.js
@@ -46,3 +46,28 @@ function showLoading() {
     }, 300);
   }, 1000);
 }
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
+
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll(".post");
+
+  posts.forEach(post => {
+    const title = post.querySelector(".post-title").innerText.toUpperCase();
+    const body = post.querySelector(".post-body").innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = "flex";
+    } else {
+      post.style.display = "none";
+    }
+  });
+}
+filter.addEventListener("input", filterPosts);
